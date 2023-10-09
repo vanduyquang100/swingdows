4acddf6f102b424cf54baf73b41bd3b9523b4a95
Duy Quang <vanduyquang2020@gmail.com>
2023-10-09 15:33:47 +0700

    Add the first FluentFrame usage

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -1,5 +1,6 @@
-import fluent.FluentFonts;
-import fluent.FluentFrame;
+import fluent.*;
+import fluent.colors.FluentColors;
+import fluent.fonts.FluentFonts;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,8 +8,19 @@ import java.awt.*;
 public class DemoApp {
     public DemoApp() {
         Runnable mainFrame = () -> {
-            new FluentFonts();
-            JFrame newFrame = new FluentFrame(Color.RED);
+            JFrame newFrame = new FluentFrame(FluentColors.FRAME_BACKGROUND, "Default Frame");
+            JPanel content = new JPanel(new FlowLayout());
+            FluentPrimaryButton button = new FluentPrimaryButton("I agree");
+            button.setPreferredSize(new Dimension(130, 32));
+            button.setText("Nah");
+            FluentButton button2 = new FluentButton("I don't");
+            button2.setPreferredSize(new Dimension(130, 32));
+            FluentTextField textField = new FluentTextField("");
+            content.add(textField);
+            content.add(button);
+            content.add(button2);
+            content.setBackground(FluentColors.FRAME_BACKGROUND);
+            newFrame.add(content);
             newFrame.setSize(1200, 720);
             newFrame.setVisible(true);
         };
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import fluent.fonts.FluentFonts;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        new FluentFonts();
         new DemoApp();
     }
 }
